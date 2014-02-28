c23086076edab24bdb247cc9a6a78a1b25669215
shantanu-vyas <shantanusvyas@gmail.com>
2014-02-28 14:24:37 -0500

    you can now control the boat via simulation

--- src/com/example/platypuscontrolapp/MapTest.java
+++ src/com/example/platypuscontrolapp/MapTest.java
@@ -10,28 +10,33 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 
-
 import android.app.Activity;
 
-import android.graphics.Color;
-import android.location.Location;
 import android.os.Bundle;
-import android.widget.Button;
+import android.os.Handler;
 import android.widget.CheckBox;
-import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MapTest extends Activity {
 	
-		SeekBar thrust = null;
-	    SeekBar rudder = null;
-	    TextView thrustProgress = null;
-	    TextView rudderProgress = null;
-	    TextView loca = null;
+		static SeekBar thrust = null;
+	    static SeekBar rudder = null;
+	    static TextView thrustProgress = null;
+	    static TextView rudderProgress = null;
+	    
+	    static TextView loca = null;
 	    CheckBox autonomous = null;
-
-
+	    static Marker boat;
+	    static LatLng pHollowStartingPoint;
+	    static double lat;
+	    static double lon;
+	    static Handler handlerThrust = new Handler();
+	    static Handler handlerRudder = new Handler();
+	    public static int thrustCurrent;
+	    public static int rudderCurrent;
+	    public static double heading = Math.PI/2.;
+	    
  	
 	@Override
 	    protected void onCreate(Bundle savedInstanceState) {
@@ -43,57 +48,82 @@ public class MapTest extends Activity {
 	        thrustProgress = (TextView)this.findViewById(R.id.getThrustProgress);
 	        rudderProgress = (TextView)this.findViewById(R.id.getRudderProgress);
 	        autonomous = (CheckBox)this.findViewById(R.id.Autonomous);
+	        loca = (TextView)this.findViewById(R.id.location);
 	        
-	        rudder.setProgress(50);
-	        seekBarValue(thrust,thrustProgress);
-	        seekBarValue(rudder,rudderProgress);
-	        
+	        updateThrust();
+	        updateRudder();
 	       
 	        // Get a handle to the Map Fragment
 	        GoogleMap map = ((MapFragment) getFragmentManager()
 	                .findFragmentById(R.id.map)).getMap();
 
-	        LatLng pittsburgh = new LatLng((float)40.436871,(float)-79.948825);
-
+	        pHollowStartingPoint = new LatLng((float)40.436871,(float)-79.948825);
+	        
+	        boat = map.addMarker(new MarkerOptions()
+	           .title("Boat 1")
+	           .snippet("IP Address: 192.168.1.1")	             
+	           .position(pHollowStartingPoint)
+	           .flat(true)
+	           .anchor(0.5f,0.5f)
+	           .rotation((float) 90.0)
+	           .icon(BitmapDescriptorFactory.fromResource(R.drawable.arrow)));
+	        
+	        lat = pHollowStartingPoint.latitude;
+	        lon = pHollowStartingPoint.longitude;
 	        map.setMyLocationEnabled(true);
-	        map.moveCamera(CameraUpdateFactory.newLatLngZoom(pittsburgh, 15));
+	        map.moveCamera(CameraUpdateFactory.newLatLngZoom(pHollowStartingPoint, 15));
 	        map.animateCamera( CameraUpdateFactory.zoomTo( 17.0f ) );
 
-	        map.addMarker(new MarkerOptions()
-	                .title("Pittsburgh")
-	                .snippet("asdfasdf pittsburgh")
-	                .position(pittsburgh));
+	        rudder.setProgress(50);
+	      //  thrust.setProgress(50);
+
+	        handlerRudder.post(new Runnable() {
+				@Override
+				public void run()
+				{
+				heading -= (rudderCurrent-50)*.001;
+				lat += Math.cos(heading)*(thrustCurrent-50)*.0000001;
+				lon += Math.sin(heading)*(thrustCurrent)*.0000001;
+				
+				boat.setPosition(new LatLng(lat,lon));							
+				loca.setText(lat + "\n" + lon);
+				boat.setRotation((float) (heading*(180/Math.PI)));
+			
+				handlerRudder.postDelayed(this,300);
+				}});
 	        
-	       map.addMarker(new MarkerOptions()
-            .position(pittsburgh)
-            .title("boat 123123")
-            .snippet("ip address: q123123"));
-           // .icon(BitmapDescriptorFactory.fromResource(R.drawable.arrow))
-            //.infoWindowAnchor(0.5f, 0.5f));
-            
+
 	  }
-	public static void seekBarValue(SeekBar seekBar, final TextView text)
+	public static void updateThrust()
 	{
-		seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+		thrust.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) 
 			{
             // TODO Auto-generated method stub
-				text.setText(String.valueOf(progress));
+				thrustCurrent = progress;
+				thrustProgress.setText(String.valueOf(progress));
 			}
-        
 			@Override
-			public void onStartTrackingTouch(SeekBar seekBar) 
-			{
-            
-            // TODO Auto-generated method stub
-			}
-        
+			public void onStartTrackingTouch(SeekBar seekBar) {}
+      
 			@Override
-			public void onStopTrackingTouch(SeekBar seekBar) 
+			public void onStopTrackingTouch(SeekBar seekBar) {}});
+	}
+	public static void updateRudder()
+	{
+		rudder.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) 
 			{
             // TODO Auto-generated method stub
+				rudderCurrent = progress;
+				rudderProgress.setText(String.valueOf(progress));
 			}
-    });
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {}
+      
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {}});
 	}
 }

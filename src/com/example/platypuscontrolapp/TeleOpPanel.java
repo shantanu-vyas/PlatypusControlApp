e43558c3f600e6b5fef42334363e04cd088085f2
shantanu-vyas <shantanusvyas@gmail.com>
2014-04-11 08:33:09 -0400

    Fixed crashing issue
    constantly making new variables

--- src/com/example/platypuscontrolapp/TeleOpPanel.java
+++ src/com/example/platypuscontrolapp/TeleOpPanel.java
@@ -21,226 +21,255 @@ import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import edu.cmu.ri.crw.AsyncVehicleServer;
 import edu.cmu.ri.crw.CrwNetworkUtils;
-import edu.cmu.ri.crw.FunctionObserver;
-import edu.cmu.ri.crw.FunctionObserver.FunctionError;
-import edu.cmu.ri.crw.VelocityListener;
 import edu.cmu.ri.crw.data.Twist;
-import edu.cmu.ri.crw.udp.UdpServer;
 import edu.cmu.ri.crw.udp.UdpVehicleServer;
 import java.net.InetSocketAddress;
 
-public class TeleOpPanel extends Activity implements OnClickListener {
-
-	private static final VelocityListener FunctionObserver = null;
-	SeekBar thrust = null;
-	SeekBar rudder = null;
-	TextView ipAddressBox = null;
-	TextView thrustProgress = null;
-	TextView rudderProgress = null;
-	LinearLayout linlay = null;
-	CheckBox autonomous = null;
-	Button mapButton = null;
-	static TextView testIP = null;
-	Thread networkThread = null;
-	TextView test = null;
-	Handler network = new Handler();
-
-	DatagramSocket socket = null;
-	DatagramPacket inPacket = null;
-	DatagramPacket outPacket = null;
-	byte[] inBuf;
-	byte[] outBuf;
-	int PORT = 8888;
-	String msg = null;
-	int a = 0;
-	String data = "";
-	String ipAddress;
-	String serverData;
-	String clientData;
-
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		this.setContentView(R.layout.teleoppanel);
-
-		ipAddressBox = (TextView) this.findViewById(R.id.printIpAddress);
-		thrust = (SeekBar) this.findViewById(R.id.thrustBar);
-		rudder = (SeekBar) this.findViewById(R.id.rudderBar);
-		linlay = (LinearLayout) this.findViewById(R.id.linlay);
-		thrustProgress = (TextView) this.findViewById(R.id.getThrustProgress);
-		rudderProgress = (TextView) this.findViewById(R.id.getRudderProgress);
-		// testIP = (TextView)this.findViewById(R.id.test);
-		mapButton = (Button) this.findViewById(R.id.mapButton);
-		autonomous = (CheckBox) this.findViewById(R.id.Autonomous);
-		test = (TextView) this.findViewById(R.id.infotest);
-		thrust.setProgress(0);
-		rudder.setProgress(50);
-		
-		new NetworkAsync().execute();
-		// serverTest();
-
-		// if (ConnectScreen.getBoatType() == true)
-		{
-
-			if (validIP(ConnectScreen.textIpAddress)) {
-				ipAddressBox.setBackgroundColor(Color.GREEN);
-			} else {
-				ipAddressBox.setBackgroundColor(Color.RED);
-			}
-			if (ConnectScreen.textIpAddress != "") {
-				ipAddressBox.setText("Boats IP: "
-						+ ConnectScreen.getIpAddress() + "");
-			}
-		}
-		// else
-		{
-			ipAddressBox.setText("Simulated Phone");
-		}
-		seekBarValue(thrust, thrustProgress);
-		seekBarValue(rudder, rudderProgress);
-		mapButton = (Button) this.findViewById(R.id.mapButton);
-		mapButton.setOnClickListener(this);
-
-	}
-
-	public static boolean validIP(String ip) {
-		if (ip == null || ip == "")
-			return false;
-		ip = ip.trim();
-		if ((ip.length() < 6) & (ip.length() > 15))
-			return false;
-
-		try {
-			Pattern pattern = Pattern
-					.compile("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
-			Matcher matcher = pattern.matcher(ip);
-			return matcher.matches();
-		} catch (PatternSyntaxException ex) {
-			return false;
-		}
-	}
-
-	public static void seekBarValue(SeekBar seekBar, final TextView text) {
-		seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-			@Override
-			public void onProgressChanged(SeekBar seekBar, int progress,
-					boolean fromUser) {
-				// TODO Auto-generated method stub
-				text.setText(String.valueOf(progress));
-			}
-
-			@Override
-			public void onStartTrackingTouch(SeekBar seekBar) {
-
-				// TODO Auto-gbenerated method stub
-			}
-
-			@Override
-			public void onStopTrackingTouch(SeekBar seekBar) {
-				// TODO Auto-generated method stub
-			}
-		});
-
-	}
-
-	public void clientTest2() {
-		
-		try {
-			a += 1;
-			// Retrieve the ServerName
-			InetAddress serverAddr = InetAddress.getByName("192.168.1.10");
-			Log.d("UDPClient", "C: Connecting...");
-			DatagramSocket socket = new DatagramSocket();
-			byte[] buf = ("Hello from Client" + a).getBytes();
-			DatagramPacket packet = new DatagramPacket(buf, buf.length,
-					serverAddr, 8888);
-			Log.d("UDPClient", "C: Sending: '" + new String(buf) + "'");
-			socket.send(packet);
-			Log.d("UDPClient", "C: Sent.");
-			Log.d("UDPClient", "C: Done.");
-		} catch (Exception e) {
-			Log.e("UDPClient", "C: Error", e);
-		}
-	}
-
-	public void serverTest() {
-
-		new Thread() {
-
-			public void run() {
-				try {
-					while (true) {
-						/* Retrieve the ServerName */
-						InetAddress serverAddr = InetAddress
-								.getByName("127.0.0.1");
-						Log.d("UDPServer", "S: Connecting...");
-						/* Create new UDP-Socket */
-						DatagramSocket socket = new DatagramSocket(8888,
-								serverAddr);
-						byte[] buf = new byte[20];
-						DatagramPacket packet = new DatagramPacket(buf,
-								buf.length);
-						Log.d("UDPServer", "S: Receiving...");
-						socket.receive(packet);
-						serverData = new String(packet.getData());
-						Log.d("UDPServer",
-								"S: Received: '" + new String(packet.getData())
-										+ "'");
-						Log.d("UDPServer", "S: Done.");
-				     	 socket.close();
-					}
-				}
-
-				catch (Exception e) {
-					System.out.println("UDP" + "S: Error" + e);
-				}
-			}
-		}.start();
-	}
-
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		startActivity(new Intent(this, MapTest.class));
-	}
-
-	
-	public void boatConnectTest()
+public class TeleOpPanel extends Activity implements OnClickListener
 	{
-		UdpVehicleServer server = new UdpVehicleServer();
-		InetSocketAddress addr = CrwNetworkUtils.toInetSocketAddress("127.0.0.1:11411");
-		server.setVehicleService(addr);
-		Twist twist = new Twist();
-		twist.dx(thrust.getProgress()*.010);
-		twist.drz(rudder.getProgress()*.010);
-		server.setVelocity(twist, null);
+
+		SeekBar thrust = null;
+		SeekBar rudder = null;
+		TextView ipAddressBox = null;
+		TextView thrustProgress = null;
+		TextView rudderProgress = null;
+		LinearLayout linlay = null;
+		CheckBox autonomous = null;
+		Button mapButton = null;
+		static TextView testIP = null;
+		Thread networkThread = null;
+		TextView test = null;
+		Handler network = new Handler();
+
+		DatagramSocket socket = null;
+		DatagramPacket inPacket = null;
+		DatagramPacket outPacket = null;
+		byte[] inBuf;
+		byte[] outBuf;
+		int PORT = 8888;
+		String msg = null;
+		int a = 0;
+		String data = "";
+		String ipAddress;
+		String serverData;
+		String clientData;
+		InetSocketAddress addr;
+
+		UdpVehicleServer server = null;
+		Twist twist = null;
 		
-	}
-	private class NetworkAsync extends AsyncTask<String, Integer, String> {
-		long oldTime = 0;
+		protected void onCreate(Bundle savedInstanceState)
+			{
+				super.onCreate(savedInstanceState);
+				this.setContentView(R.layout.teleoppanel);
+				initBoat();
+				ipAddressBox = (TextView) this.findViewById(R.id.printIpAddress);
+				thrust = (SeekBar) this.findViewById(R.id.thrustBar);
+				rudder = (SeekBar) this.findViewById(R.id.rudderBar);
+				linlay = (LinearLayout) this.findViewById(R.id.linlay);
+				thrustProgress = (TextView) this.findViewById(R.id.getThrustProgress);
+				rudderProgress = (TextView) this.findViewById(R.id.getRudderProgress);
+				// testIP = (TextView)this.findViewById(R.id.test);
+				mapButton = (Button) this.findViewById(R.id.mapButton);
+				autonomous = (CheckBox) this.findViewById(R.id.Autonomous);
+				test = (TextView) this.findViewById(R.id.infotest);
+				thrust.setProgress(0);
+				rudder.setProgress(50);
 
-		@Override
-		protected String doInBackground(String... arg0) {
-			while (true) {
-				if (System.currentTimeMillis() % 100 == 0
-						&& oldTime != System.currentTimeMillis()) {
-					//clientTest2();
-					boatConnectTest();
-					oldTime = System.currentTimeMillis();
-					a += 1;
-					publishProgress();
-				}
+				new NetworkAsync().execute();
+				// serverTest();
+
+				if (ConnectScreen.getBoatType() == true)
+					{
+
+						if (validIP(ConnectScreen.textIpAddress))
+							{
+								ipAddressBox.setBackgroundColor(Color.GREEN);
+							} else
+							{
+								ipAddressBox.setBackgroundColor(Color.RED);
+							}
+						if (ConnectScreen.textIpAddress != "")
+							{
+								ipAddressBox.setText("Boats IP: " + ConnectScreen.getIpAddress() + "");
+							}
+					}
+				if (ConnectScreen.getBoatType() == false)
+					{
+						ipAddressBox.setText("Simulated Phone");
+					}
+				seekBarValue(thrust, thrustProgress);
+				seekBarValue(rudder, rudderProgress);
+				mapButton = (Button) this.findViewById(R.id.mapButton);
+				mapButton.setOnClickListener(this);
+
 			}
-		}
+
+		public static boolean validIP(String ip)
+			{
+				if (ip == null || ip == "")
+					return false;
+				ip = ip.trim();
+				if ((ip.length() < 6) & (ip.length() > 15))
+					return false;
+
+				try
+					{
+						Pattern pattern = Pattern
+								.compile("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
+						Matcher matcher = pattern.matcher(ip);
+						return matcher.matches();
+					} catch (PatternSyntaxException ex)
+					{
+						return false;
+					}
+			}
+
+		public static void seekBarValue(SeekBar seekBar, final TextView text)
+			{
+				seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+					{
+						@Override
+						public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+							{
+								// TODO Auto-generated method stub
+								text.setText(String.valueOf(progress));
+							}
+
+						@Override
+						public void onStartTrackingTouch(SeekBar seekBar)
+							{
+
+								// TODO Auto-gbenerated method stub
+							}
+
+						@Override
+						public void onStopTrackingTouch(SeekBar seekBar)
+							{
+								// TODO Auto-generated method stub
+							}
+					});
+
+			}
+
+		public void clientTest2()
+			{
+
+				try
+					{
+						a += 1;
+						// Retrieve the ServerName
+						InetAddress serverAddr = InetAddress.getByName("192.168.1.10");
+						Log.d("UDPClient", "C: Connecting...");
+						DatagramSocket socket = new DatagramSocket();
+						byte[] buf = ("Hello from Client" + a).getBytes();
+						DatagramPacket packet = new DatagramPacket(buf, buf.length, serverAddr, 8888);
+						Log.d("UDPClient", "C: Sending: '" + new String(buf) + "'");
+						socket.send(packet);
+						Log.d("UDPClient", "C: Sent.");
+						Log.d("UDPClient", "C: Done.");
+						socket.close();
+					} catch (Exception e)
+					{
+						Log.e("UDPClient", "C: Error", e);
+					}
+			}
+
+		public void serverTest()
+			{
+				new Thread()
+					{
+						public void run()
+							{
+								try
+									{
+										while (true)
+											{
+												/* Retrieve the ServerName */
+												InetAddress serverAddr = InetAddress.getByName("127.0.0.1");
+												Log.d("UDPServer", "S: Connecting...");
+												/* Create new UDP-Socket */
+												DatagramSocket socket = new DatagramSocket(8888, serverAddr);
+												byte[] buf = new byte[20];
+												DatagramPacket packet = new DatagramPacket(buf, buf.length);
+												Log.d("UDPServer", "S: Receiving...");
+												socket.receive(packet);
+												serverData = new String(packet.getData());
+												Log.d("UDPServer", "S: Received: '" + new String(packet.getData()) + "'");
+												Log.d("UDPServer", "S: Done.");
+												socket.close();
+											}
+									}
+
+								catch (Exception e)
+									{
+										System.out.println("UDP" + "S: Error" + e);
+									}
+							}
+					}.start();
+			}
 
 		@Override
-		protected void onProgressUpdate(Integer... result) {
-			test.setText("" + a + "\n" + serverData); // TODO Auto-generated
-														// method stub
-		}
+		public void onClick(View v)
+			{
+				// TODO Auto-generated method stub
+				startActivity(new Intent(this, MapTest.class));
+			}
 
-	}
-	
+		public void initBoat()
+			{
+				server = new UdpVehicleServer();
+				if (ConnectScreen.getIpAddress() != null || ConnectScreen.getIpAddress() != "")
+					{
+						addr = CrwNetworkUtils.toInetSocketAddress(ConnectScreen.getIpAddress() + ":11411");
+					}
+				else
+					{
+						addr = CrwNetworkUtils.toInetSocketAddress("127.0.0.1:11411");
+					}
+				server.setVehicleService(addr);
+				twist = new Twist();
+			}
 
-}// class
+		public void boatConnectTest()
+			{
+				
+				twist.dx(thrust.getProgress() * .010);
+				twist.drz(rudder.getProgress() * .010);
+				server.setVelocity(twist, null);
+
+			}
+
+		private class NetworkAsync extends AsyncTask<String, Integer, String>
+			{
+				long oldTime = 0;
+
+				@Override
+				protected String doInBackground(String... arg0)
+					{
+						while (true)
+							{
+								if (System.currentTimeMillis() % 100 == 0 && oldTime != System.currentTimeMillis())
+									{
+										// clientTest2();
+										boatConnectTest();
+										oldTime = System.currentTimeMillis();
+										a += 1;
+										publishProgress();
+									}
+							}
+					}
+
+				@Override
+				protected void onProgressUpdate(Integer... result)
+					{
+						test.setText("" + a + "\n" + serverData); // TODO
+																	// Auto-generated
+																	// method
+																	// stub
+					}
+
+			}
+
+	}// class
